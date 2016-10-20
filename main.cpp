b278ec9b92a5b5a0e1090c09074ce5f77863d789
maxmaxjian <jianw812@gmail.com>
2016-10-19 19:39:35 -0700

    finished version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <unordered_set>
+#include <functional>
 
 using std::vector;
 
@@ -29,45 +31,137 @@ struct pos {
     size_t x, y;
     pos(size_t xval, size_t yval) : x(xval), y(yval){}
 
-    bool operator==(const pos & other) {
+    bool operator==(const pos & other) const {
         return x == other.x && y == other.y;
     }
 
-    bool operator!=(const pos & other) {
+    bool operator!=(const pos & other) const {
         return !(*this==other);
     }
+
+  void print() const {
+    std::cout << "(" << x << "," << y << ")";
+  }
 };
 
-template<>
-struct hash<pos> {
-    size_t operator()(const pos & p) {
-        return std::hash<size_t>()(p.x)^std::hash<size_t>()(p.y);
+
+// struct posHasher {
+//   size_t operator()(const pos & p) const {
+//     return std::hash<size_t>()(p.x)^std::hash<size_t>()(p.y);
+//   }
+// };
+
+namespace std {
+  template<>
+  struct hash<pos> {
+    size_t operator()(const pos & p) const {
+      return std::hash<size_t>()(p.x)^std::hash<size_t>()(p.y);
     }
-};
+  };
+}
 
 class solution {
   public:
     int trapRainWater(const vector<vector<int>> & heightMap) {
       int maxht = maxInMat(heightMap);
       vector<vector<vector<int>>> cross;
-      for (int i = 2; i <= maxht; ++i)
+      for (int i = 1; i <= maxht; ++i)
 	cross.push_back(crossCut(heightMap, i));
 
       int water = 0;
-      for (size_t i = 0; i < cross.size(); ++i)
+      for (size_t i = 0; i < cross.size(); ++i) {
+	// std::cout << i << "th layer:\n";
 	water += enclosedWater(cross[i]);
+      }
 
       return water;
     }
 
 private:
   int enclosedWater(const vector<vector<int>> & grid) {
-      std::unordered_set<pos> cands;
-      for (size_t i = 1; i < grid.size()-1; ++i) {
-          for (size_t j = 1; j < grid[i].size()-1; ++j)
-              if (!grid[i][j])
-                  cands.insert(pos(i,j));
-      }
+    std::unordered_set<pos> cands;
+    for (size_t i = 1; i < grid.size()-1; ++i)
+      for (size_t j = 1; j < grid[i].size()-1; ++j)
+	if (!grid[i][j])
+	  cands.insert(pos(i,j));
+    // std::cout << "size of cands = " << cands.size() << std::endl;
+
+    vector<std::unordered_set<pos>> grps;
+    while (!cands.empty()) {
+      auto it = cands.begin();
+      std::unordered_set<pos> curr{*it};
+      cands.erase(it);
+      auto temp = addNeibs(grid, cands, curr);
+      // std::for_each(temp.begin(), temp.end(), [](const pos & p){p.print();});
+      // std::cout << std::endl;
+      grps.push_back(temp);
+      for (auto p : temp)
+	cands.erase(p);
+      // std::cout << "size of cands = " << cands.size() << std::endl;
+    }
+    // std::cout << "number of grps = " << grps.size() << std::endl;
+
+    int water = 0;
+    for (auto & grp : grps) {
+      if (isPond(grid, grp))
+	water += grp.size();
+    }
+    
+    return water;
+  }
+
+  bool isPond(const vector<vector<int>> & grid, const std::unordered_set<pos> & grps) {
+    for (auto it = grps.begin(); it != grps.end(); ++it) {
+      pos up(it->x-1, it->y);
+      if (grps.find(up) == grps.end() && grid[up.x][up.y] == 0)
+	return false;
+      pos down(it->x+1, it->y);
+      if (grps.find(down) == grps.end() && grid[down.x][down.y] == 0)
+	return false;
+      pos left(it->x, it->y-1);
+      if (grps.find(left) == grps.end() && grid[left.x][left.y] == 0)
+	return false;
+      pos right(it->x, it->y+1);
+      if (grps.find(right) == grps.end() && grid[right.x][right.y] == 0)
+	return false;
+    }
+    return true;
+  }
+
+  std::unordered_set<pos> addNeibs(const vector<vector<int>> & grid, std::unordered_set<pos> & cands, const std::unordered_set<pos> & curr) {
+    std::unordered_set<pos> result;
+    vector<pos> next = getNext(grid, cands, curr);
+    if (next.empty())
+      result = curr;
+    else {
+      auto temp = curr;
+      temp.insert(next.begin(), next.end());
+      auto cpy = cands;
+      for (auto p : next)
+	cpy.erase(p);
+      result = addNeibs(grid, cpy, temp);
+    }
+    return result;
+  }
+
+  vector<pos> getNext(const vector<vector<int>> & grid, const std::unordered_set<pos> & cands, const std::unordered_set<pos> & curr) {
+    vector<pos> next;
+    for (auto it = curr.begin(); it != curr.end(); ++it) {
+      pos left(it->x-1, it->y);
+      if (cands.find(left) != cands.end() && curr.find(left) == curr.end() && grid[left.x][left.y] == 0)
+	next.push_back(left);
+      pos right(it->x+1, it->y);
+      if (cands.find(right) != cands.end() && curr.find(right) == curr.end()  && grid[right.x][right.y] == 0)
+	next.push_back(right);
+      pos up(it->x, it->y-1);
+      if (cands.find(up) != cands.end() && curr.find(up) == curr.end() && grid[up.x][up.y] == 0)
+	next.push_back(up);
+      pos down(it->x, it->y+1);
+      if (cands.find(down) != cands.end() && curr.find(down) == curr.end() && grid[down.x][down.y] == 0)
+	next.push_back(down);
+    }
+    
+    return next;
   }
   
   int maxInMat(const vector<vector<int>> & grid) {
